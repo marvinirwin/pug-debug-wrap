5e7426a0c1d2818cd508e42b192e60edd58ec487
marvinirwin <marvin@marvinirwin.com>
2023-02-23 13:57:34 +0800

    updated package name and added console log to wrap

--- packages/pug-runtime/wrap.js
+++ packages/pug-runtime/wrap.js
@@ -3,8 +3,10 @@ var runtime = require('./');
 module.exports = wrap;
 function wrap(template, templateName) {
   templateName = templateName || 'template';
+  let args = template + '\n' + 'return ' + templateName + ';';
+  console.log(args)
   return Function(
     'pug',
-    template + '\n' + 'return ' + templateName + ';'
+    args
   )(runtime);
 }
